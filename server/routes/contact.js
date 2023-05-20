b12d3d03680849df58d09eb9a65370cbdd6e453e
Shawn <jaynielsingh@yahoo.com>
2023-05-20 16:31:20 -0700

    update json

--- server/routes/contact.js
+++ server/routes/contact.js
@@ -4,11 +4,13 @@ const express = require("express");
 var router = express.Router();
 const nodemailer = require("nodemailer");
 
-router.get("/contact", async (req, res, next) => {
+router.get("/contact", async (req, res, next) =>
+{
   res.send("This is the contact route ");
 });
 
-router.post("/contact", async (req, res, next) => {
+router.post("/contact", async (req, res, next) =>
+{
   const { name, email, message } = req.body;
   console.log(req.body);
   console.log(name, email, message);
@@ -23,11 +25,14 @@ router.post("/contact", async (req, res, next) => {
     auth: {
       user: process.env.MY_EMAIL,
       pass: process.env.MY_PASSWORD,
+
     },
     tls: {
       ciphers: "SSLv3",
     },
   });
+  console.log(MY_EMAIL, MY_PASSWORD);
+
 
   const info = await transporter.sendMail({
     from: process.env.MY_EMAIL,
@@ -36,7 +41,6 @@ router.post("/contact", async (req, res, next) => {
     text: `Name: ${name} \nEmail: ${email} \nMessage: ${message}`,
   });
   console.log("Message Sent: %s", info.messageId);
-
 
 
   // res.json(message);
